93cf58943fe371c02472daf234fdc11f94a9263b
LSG <sugyeong9611@gmail.com>
2025-07-15 15:22:10 +0900

    main-page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,6 @@
-"use client";
-
 import MainPage from "@/components/MainPage";
-// import { darkMode, lightMode } from "@/store/actions/colorModeActions";
-import { AppDispatch, RootState } from "@/store/store";
-import { useSelector, useDispatch } from "react-redux";
 
 export default function Page() {
-  // const colorMode = useSelector((state: RootState) => state.colorMode.mode);
-  const dispatch = useDispatch<AppDispatch>();
-
   return (
     <div className="font-noto-sans">
       <MainPage />
